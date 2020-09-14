7b3d12b3db698c912a90f0ae07a50d985087e83c
Jason Daniels <jason.c.daniels@gmail.com>
2020-09-14 07:00:25 -0500

    * implmented validation. * published to docs.

--- src/model/gameInfo.js
+++ src/model/gameInfo.js
@@ -1,17 +1,5 @@
-<style>
-    @import "Worksheet.css";
-</style>
-
-<script>
-    import GameInfo from '../shared/GameInfo';
-    import LogoBar from '../shared/LogoBar';
-    import Issues from "./Issues";
-    import FacesAndPlaces from "./FacesAndPlaces";
-    import Dials from "./Dials/Dials.svelte";
-    import Skills from "./Skills/Skills.svelte";
-    import StuntsAndExtras from "./StuntsAndExtras/StuntsAndExtras.svelte";
-
-    export let gameWorksheet = {
+export default function getNewGameInfo() {
+    return {
         name: "",
         setting: "",
         currentIssues: "",
@@ -58,17 +46,5 @@
             defaultNumberOfStressBoxes: 2,
             defaultConsequenceSlots: "2/4/6"
         }
-    }
-</script>
-
-<div class="page" >
-    <LogoBar />
-    <GameInfo bind:name={gameWorksheet.name} bind:setting={gameWorksheet.setting}  />
-    <Issues bind:currentIssues={gameWorksheet.currentIssues} bind:impendingIssues={gameWorksheet.impendingIssues} />
-    <FacesAndPlaces bind:facesAndPlaces={gameWorksheet.facesAndPlaces} />
-    <div class="flex-row" style="margin-top: 0.5rem; margin-bottom: 0.1rem">
-        <Dials bind:dials={gameWorksheet.dials} props={{class:"flex-item-1", style:"margin-right:0.5rem;"}}  />
-        <Skills bind:skills={gameWorksheet.skills} props={{class:"flex-item-1"}} />
-    </div>
-    <StuntsAndExtras bind:stuntsAndExtras={gameWorksheet.stuntsAndExtras} />
-</div>
+    };
+}
