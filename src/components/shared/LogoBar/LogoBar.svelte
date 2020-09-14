5baedca98ca7acafc500bc0e744af5f50b0c3524
Jason Daniels <jason.c.daniels@gmail.com>
2020-09-14 13:03:01 -0500

    * corrected the logo to comply with licensing, pending color change approval * published to docs

--- src/components/shared/LogoBar/LogoBar.svelte
+++ src/components/shared/LogoBar/LogoBar.svelte
@@ -6,6 +6,6 @@
     import Logo from "./logo.svg"
 </script>
 <div class="logo">
-    <Logo height="100%" width="100%" />
+    <Logo height="100%" width="100%" style="fill:black" />
 </div>
 <h2>Game Creation Worksheet</h2>
